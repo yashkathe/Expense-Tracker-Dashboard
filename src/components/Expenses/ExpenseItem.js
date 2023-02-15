20534d90955ba4bc2b0dcc29a3d3a16b5c29f09c
Yash Kathe <yashkathe123@gmail.com>
2023-02-15 21:54:51 +0530

    created basic react app with expenseitem, expensedate and UI card component

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import styles from './ExpenseItem.module.css';
+
+const ExpenseItem = (props) => {
+    return (
+        <Card className={ styles[ 'expense-item' ] }>
+            <ExpenseDate date={ props.date } />
+            <div className={ styles[ 'expense-item__description' ] }>
+                <h2>{ props.title }</h2>
+                <div className={ styles[ 'expense-item__price' ] }>${ props.amount }</div>
+            </div>
+        </Card>
+    );
+};
+
+export default ExpenseItem;
